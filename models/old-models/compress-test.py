25b9aed53a839f749161298f14d54fe65e3e1bbf
symonoliver <symon@alsocollective.com>
2017-05-28 20:23:16 -0400

    completed wave height model generator for erie only

--- models/old-models/compress-test.py
+++
@@ -1,4 +0,0 @@
-import SVGCompress
-import shapely
-
-SVGCompress.compress_by_method(filename = 'waves0.svg', optimize = True, optimize_options='optimize_tothe_max', compression_type = 'simplify', epsilon = 5, selection_tuple = ('bboxarea', 600), curve_fidelity = 1, pre_select = True, operation_key = 'hull')
